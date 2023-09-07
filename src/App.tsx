a7f9333557e5b6da8fda99ddbc59d82a08a7103a
vishu39 <vipulsharmak@gmail.com>
2023-09-07 16:37:26 +0530

    fix router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import "./App.scss";
 import Layout from "./Component/Layout/Layout";
 
 function App() {
   return (
     <div className="app-main">
-      <BrowserRouter>
+      <HashRouter>
         <Layout />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
