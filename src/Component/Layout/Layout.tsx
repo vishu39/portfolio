0ea6562e9b2878a83a150ce4c4cefb790089931e
vishu39 <vipulsharmak@gmail.com>
2024-08-09 17:38:28 +0530

    new build

--- src/Component/Layout/Layout.tsx
+++ src/Component/Layout/Layout.tsx
@@ -1,20 +1,52 @@
 // import { useEffect, useState } from 'react'
+import { useEffect } from 'react';
+import useWindowSize from '../../Hooks/useWindowSize';
 import './layout.scss';
-import { DownOutlined } from '@ant-design/icons';
+import { DownOutlined, FacebookOutlined, HistoryOutlined, HomeOutlined, InstagramOutlined, LinkedinOutlined, MailOutlined, MenuOutlined, MergeOutlined, SnippetsOutlined } from '@ant-design/icons';
+import { Dropdown, MenuProps } from 'antd';
+
+const items: MenuProps['items'] = [
+  {
+    label: <div style={{ marginLeft: "24px" }}><HomeOutlined style={{ marginRight: "12px" }} /> Home</div>,
+    key: '0',
+  },
+  {
+    type: 'divider',
+  },
+  {
+    label: <div style={{ marginLeft: "24px" }}><MergeOutlined style={{ marginRight: "12px" }} /> About</div>,
+    key: '1',
+  },
+  {
+    type: 'divider',
+  },
+  {
+    label: <div style={{ marginLeft: "24px" }}><HistoryOutlined style={{ marginRight: "12px" }} /> Services</div>,
+    key: '2',
+  },
+  {
+    type: 'divider',
+  },
+  {
+    label: <div style={{ marginLeft: "24px" }}><SnippetsOutlined style={{ marginRight: "12px" }} /> Resume</div>,
+    key: '3',
+  },
+  {
+    type: 'divider',
+  },
+  {
+    label: <div style={{ marginLeft: "24px" }}><MailOutlined style={{ marginRight: "12px" }} /> Contact</div>,
+    key: '4',
+  },
+];
 
 const Layout = () => {
-  // const [collapsed, setCollapsed] = useState(JSON.parse(localStorage.getItem("isCollapsed")));
 
-  // const toggleSidebar = () => {
-  //   // setCollapsed(!collapsed);
-  //   let collapse: boolean = !collapsed
-  //   localStorage.setItem("isCollapsed", JSON.stringify(collapse))
-  // };
+  const { width } = useWindowSize();
 
-  // useEffect(() => {
-  //   let collapse = localStorage.getItem('isCollapsed')
-  //   console.log(collapse);
-  // }, [collapsed])
+  useEffect(() => {
+  }, [width])
+
 
   return (
     <div className='main hide-scrollbar'>
@@ -27,20 +59,29 @@ const Layout = () => {
               margin: "0"
             }}>Portfolio</h2>
           </div>
-          <div className='navList' style={{
-            width: "60%"
-          }}>
-            <span>Home</span>
-            <span>About</span>
-            <span>Services</span>
-            <span>Resume</span>
-            <span>Contact</span>
-          </div>
+
+          {width > 600 ?
+            <div className='navList' style={{
+              width: "60%"
+            }}>
+              <span>Home</span>
+              <span>About</span>
+              <span>Services</span>
+              <span>Resume</span>
+              <span>Contact</span>
+            </div>
+            : <div>
+              <Dropdown overlayStyle={{ width: '50%' }} menu={{ items }} trigger={['click']}>
+                <MenuOutlined onClick={(e) => e.preventDefault()} />
+              </Dropdown>
+            </div>
+          }
+
         </div>
       </header>
       <div className="content">
         <div id='homeMain'>
-          <p> HELLO, WORLD.</p>
+          <p style={{ color: "#cc005f" }}> HELLO, WORLD.</p>
           <h1>I'm Vishu Sharma.</h1>
           <div>
             <p>| Front-End Devloper |</p>
@@ -49,6 +90,13 @@ const Layout = () => {
           <div className='btnDiv'>
             <button className='moreAboutMeBtn'>More About Me <DownOutlined className='icon' /></button>
           </div>
+
+          <div style={{ marginTop: "80px", display: "flex", justifyContent: "space-evenly", width: "100%" }}>
+            <FacebookOutlined style={{ color: "#cc005f", cursor: "pointer", fontSize: "30px", borderRadius: "5px" }} />
+            <InstagramOutlined style={{ color: "#cc005f", cursor: "pointer", fontSize: "30px", borderRadius: "5px" }} />
+            <LinkedinOutlined style={{ color: "#cc005f", cursor: "pointer", fontSize: "30px", borderRadius: "5px" }} />
+          </div>
+
         </div>
       </div>
     </div>
---
+++ src/Hooks/useWindowSize.tsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+function useWindowSize() {
+    const [size, setSize] = useState({ width: window.innerWidth, height: window.innerHeight });
+
+    useEffect(() => {
+        const handleResize = () => {
+            setSize({ width: window.innerWidth, height: window.innerHeight });
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        // Cleanup event listener on component unmount
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return size;
+}
+
+export default useWindowSize;
