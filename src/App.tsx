d410826adb2404d8dd6990b1edcb7fb529455b0d
vishu39 <vipulsharmak@gmail.com>
2024-08-09 16:56:54 +0530

    new ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { HashRouter } from "react-router-dom";
+// import { HashRouter } from "react-router-dom";
 import "./App.scss";
 import Layout from "./Component/Layout/Layout";
 
 function App() {
   return (
-    <div className="app-main">
-      <HashRouter>
-        <Layout />
-      </HashRouter>
+    <div className="app">
+      {/* <HashRouter> */}
+      <Layout></Layout>
+      {/* </HashRouter> */}
     </div>
   );
 }
--- src/Component/Layout/Layout.tsx
+++ src/Component/Layout/Layout.tsx
@@ -1,53 +1,58 @@
-import { useEffect, useState } from "react";
-import Sidebar from "../Sidebar/Sidebar";
-import Pages from "../../pages";
+import { useEffect, useState } from 'react'
+import './layout.scss';
+import { DownOutlined } from '@ant-design/icons';
 
-function Layout() {
-  const [isOpen, setIsOpen] = useState(true);
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+const Layout = () => {
+  const [collapsed, setCollapsed] = useState(JSON.parse(localStorage.getItem("isCollapsed")));
 
   const toggleSidebar = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const handleResize = () => {
-    setWindowWidth(window.innerWidth);
+    // setCollapsed(!collapsed);
+    let collapse: boolean = !collapsed
+    localStorage.setItem("isCollapsed", JSON.stringify(collapse))
   };
 
   useEffect(() => {
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (windowWidth <= 700) {
-      setIsOpen(false);
-    } else {
-      setIsOpen(true);
-    }
-  }, [windowWidth]);
+    let collapse = localStorage.getItem('isCollapsed')
+    console.log(collapse);
+  }, [collapsed])
 
   return (
-    <div style={{ display: "flex", width: "100%", height: "100%" }}>
-      <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
-      <div
-        style={
-          isOpen
-            ? {
-                width: "85%",
-              }
-            : {
-                width: "95%",
-              }
-        }
-        className="content"
-      >
-        <Pages />
+    <div className='main hide-scrollbar'>
+      <header>
+        <div className="nav">
+          <div style={{
+            width: "40%"
+          }}>
+            <h2 style={{
+              margin: "0"
+            }}>Portfolio</h2>
+          </div>
+          <div className='navList' style={{
+            width: "60%"
+          }}>
+            <span>Home</span>
+            <span>About</span>
+            <span>Services</span>
+            <span>Resume</span>
+            <span>Contact</span>
+          </div>
+        </div>
+      </header>
+      <div className="content">
+        <div id='homeMain'>
+          <p> HELLO, WORLD.</p>
+          <h1>I'm Vishu Sharma.</h1>
+          <div>
+            <p>| Front-End Devloper |</p>
+          </div>
+
+          <div className='btnDiv'>
+            <button className='moreAboutMeBtn'>More About Me <DownOutlined className='icon' /></button>
+          </div>
+        </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Layout;
+export default Layout
