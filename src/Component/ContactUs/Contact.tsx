68e03b07d30a1c11a3422f7051ddfda47e72d931
vishu 39 <vipulsharmak@gmail.com>
2025-08-15 22:21:37 +0530

    chanegs

--- src/Component/ContactUs/Contact.tsx
+++ src/Component/ContactUs/Contact.tsx
@@ -1,12 +1,127 @@
 import { forwardRef } from "react";
 import "./contact.scss";
+import {
+  PhoneOutlined,
+  MailOutlined,
+  EnvironmentOutlined,
+} from "@ant-design/icons";
+
+import { Form, Input, Button } from "antd";
 
 const Contact = forwardRef<HTMLDivElement, any>((_props, ref) => {
-    return (
-        <div ref={ref} id="contactMain">
-            Contact
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    console.log("Form Data:", values);
+  };
+
+  return (
+    <div ref={ref} id="contactMain">
+      <div className="grid1x2">
+        <div className="left">
+          <div className="addressDiv">
+            <div>
+              <EnvironmentOutlined className="commonIconCss" />
+            </div>
+            <div className="addressText">
+              <h4>Address</h4>
+              <span>Modinagar, Ghaziabad, Uttar Pradesh 201204</span>
+            </div>
+          </div>
+
+          <div className="phoneDiv">
+            <div>
+              <PhoneOutlined className="commonIconCss" />
+            </div>
+            <div className="phoneText">
+              <h4>Phone</h4>
+              <span>+91 9997048252</span>
+            </div>
+          </div>
+
+          <div className="mailDiv">
+            <div>
+              <MailOutlined className="commonIconCss" />
+            </div>
+            <div className="mailText">
+              <h4>Mail</h4>
+              <span>vipulsharmak@gmail.com</span>
+            </div>
+          </div>
         </div>
-    );
+
+        <div className="right">
+          <div className="contactCard">
+            <h2 className="heading">Send Message</h2>
+
+            <div className="contactForm">
+              <Form form={form} layout="vertical" onFinish={onFinish}>
+                <Form.Item
+                  label="First Name"
+                  name="firstName"
+                  rules={[
+                    { required: true, message: "Please enter your first name" },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
+
+                <Form.Item
+                  label="Last Name"
+                  name="lastName"
+                  rules={[
+                    { required: true, message: "Please enter your last name" },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
+
+                <Form.Item
+                  label="Phone"
+                  name="phone"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please enter your phone number",
+                    },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
+
+                <Form.Item
+                  label="Email"
+                  name="email"
+                  rules={[
+                    { required: true, message: "Please enter your email" },
+                    { type: "email", message: "Please enter a valid email" },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
+
+                <Form.Item
+                  label="Message"
+                  name="message"
+                  rules={[
+                    { required: true, message: "Please enter your message" },
+                  ]}
+                >
+                  <Input.TextArea rows={4} />
+                </Form.Item>
+
+                <Form.Item>
+                  <Button type="primary" htmlType="submit">
+                    Submit
+                  </Button>
+                </Form.Item>
+              </Form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 });
 
 export default Contact;
--- src/Component/Home/Home.tsx
+++ src/Component/Home/Home.tsx
@@ -7,7 +7,29 @@ import {
 import "./home.scss";
 import { forwardRef } from "react";
 
+// import { useNavigate } from "react-router-dom";
+
 const Home = forwardRef<HTMLDivElement, {}>((_props, ref) => {
+  // const navigate = useNavigate();
+
+  const navigateToMediaPages = (name: string) => {
+    if (name === "facebook") {
+      window.open("https://www.facebook.com/share/1Vwd7befMa/", "_blank");
+    }
+    if (name === "instagram") {
+      window.open(
+        "https://www.instagram.com/vishusharmak?igsh=MWdmbmJycGlrbTd2bA==",
+        "_blank"
+      );
+    }
+    if (name === "linkedin") {
+      window.open(
+        "https://www.linkedin.com/in/vishu-sharma-0628501b6",
+        "_blank"
+      );
+    }
+  };
+
   return (
     <div ref={ref} id="homeMain">
       <p style={{ color: "#cc005f" }}> HELLO, WORLD.</p>
@@ -31,28 +53,16 @@ const Home = forwardRef<HTMLDivElement, {}>((_props, ref) => {
         }}
       >
         <FacebookOutlined
-          style={{
-            color: "#cc005f",
-            cursor: "pointer",
-            fontSize: "30px",
-            borderRadius: "5px",
-          }}
+          className="commonIconCss"
+          onClick={() => navigateToMediaPages("facebook")}
         />
         <InstagramOutlined
-          style={{
-            color: "#cc005f",
-            cursor: "pointer",
-            fontSize: "30px",
-            borderRadius: "5px",
-          }}
+          className="commonIconCss"
+          onClick={() => navigateToMediaPages("instagram")}
         />
         <LinkedinOutlined
-          style={{
-            color: "#cc005f",
-            cursor: "pointer",
-            fontSize: "30px",
-            borderRadius: "5px",
-          }}
+          className="commonIconCss"
+          onClick={() => navigateToMediaPages("linkedin")}
         />
       </div>
     </div>
